9f1b8c072544e9dabc607cbf92fe2d99325d0bae
palash04 <palashkamble22@gmail.com>
2020-04-07 20:09:31 +0530

    Update code.cpp

--- CycleDetection/DirectedGraph/code.cpp
+++ CycleDetection/DirectedGraph/code.cpp
@@ -1,1 +1,82 @@
-..
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph{
+    int V;
+    vector<vector<int>> adj;
+public:
+    Graph(int V){
+        this->V = V;
+        adj.resize(V);
+    }
+    void addEdge(int u,int v){
+        adj[u].push_back(v);
+    }
+    
+    bool isCycleUtil(int u,vector<bool> &visited,vector<bool> &recVisited){
+        stack<int> st;
+        st.push(u);
+        
+        while (!st.empty()){
+            
+            u = st.top();
+            
+            if (!visited[u]){
+                visited[u] = true;
+                recVisited[u] = true;
+            }
+            
+            bool isAnyChildrenNotVisited = false;
+            for (auto v=adj[u].begin(); v!=adj[u].end(); v++){
+                if (recVisited[*v]){
+                    return true;
+                }
+                if (!visited[*v]){
+                    st.push(*v);
+                    isAnyChildrenNotVisited = true;
+                    break;
+                }
+            }
+            if (!isAnyChildrenNotVisited){
+                recVisited[u] = false;
+                st.pop();
+            }
+        }
+        return false;
+    }
+    
+    bool isCycle(){
+        vector<bool> visited(V);
+        vector<bool> recVisited(V);
+        
+        for (int i=0;i<V;i++){
+            if (!visited[i]){
+                if (isCycleUtil(i,visited,recVisited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+};
+
+int main(){
+    Graph g(12);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 5);
+    g.addEdge(2, 4);
+    g.addEdge(4, 11);
+    g.addEdge(5, 8);
+    g.addEdge(3, 7);
+    g.addEdge(3, 6);
+    g.addEdge(8, 10);
+    g.addEdge(8, 9);
+    g.addEdge(9, 10);
+    g.addEdge(11, 2);   // cycle exists for this edge
+    
+    
+    cout << g.isCycle() << "\n";
+    
+}
