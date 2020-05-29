9938bbb8a56e9a627e761a6aab927917a7f47a55
palash04 <palashkamble22@gmail.com>
2020-05-29 19:56:14 +0530

    Update code.cpp

--- HeapSort/code.cpp
+++ HeapSort/code.cpp
@@ -1,46 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void heapify(vector<int> &nums,int i,int n){
+
+void printNums(vector<int> &nums){
+    for (auto num : nums){
+        cout << num << " ";
+    }
+    cout << "\n";
+}
+
+void maxHeapify(vector<int> &nums,int i,int n){
     int l = 2*i + 1;
     int r = 2*i + 2;
     int largest = i;
+    
     if (l < n && nums[l] > nums[largest]){
         largest = l;
     }
-    if(r < n && nums[r] > nums[largest]){
+    if (r < n && nums[r] > nums[largest]){
         largest = r;
     }
-    
     if (largest != i){
-        swap(nums[i],nums[largest]);
-        heapify(nums, largest, n);
+        swap(nums[i], nums[largest]);
+        maxHeapify(nums, largest, n);
     }
 }
 
-void heapSort(vector<int> &nums,int n){
-    // build maxHeap
-    for (int i=n/2-1; i>=0; i--){
-        heapify(nums, i, n);
+void buildMaxHeap(vector<int> &nums,int n){
+    for (int i=n/2-1;i>=0;i--){
+        maxHeapify(nums, i, n);
     }
-    
+}
+
+void heapSortAscending(vector<int> &nums,int n){
+    buildMaxHeap(nums,n);
     for (int i=n-1;i>=0;i--){
         swap(nums[0], nums[i]);
-        heapify(nums, 0, i);
+        maxHeapify(nums, 0, i);
     }
-    
 }
 
+void minHeapify(vector<int> &nums,int i,int n){
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    int smallest = i;
+    
+    if (l < n && nums[l] < nums[smallest]){
+        smallest = l;
+    }
+    if (r < n && nums[r] < nums[smallest]){
+        smallest = r;
+    }
+    if (smallest != i){
+        swap(nums[i],nums[smallest]);
+        minHeapify(nums, smallest, n);
+    }
+}
+
+void buildMinHeap(vector<int> &nums,int n){
+    for (int i=n/2-1;i>=0;i--){
+        minHeapify(nums, i, n);
+    }
+}
+
+void heapSortDescending(vector<int> &nums,int n){
+    buildMinHeap(nums, n);
+    for (int i=n-1;i>=0;i--){
+        swap(nums[0], nums[i]);
+        minHeapify(nums, 0, i);
+    }
+}
 
 int main(){
-    vector<int> nums = {15,14,13,12,11,10,9,8,7,6,5,4,3};
+    vector<int> nums = {4,2,1,7,9,8,3,6,0,5};
     int n = (int)nums.size();
-    heapSort(nums,n);
-    
-    for (int i=0;i<n;i++){
-        cout << nums[i] << " ";
-    }
-    
-    cout << "\n";
-    
+    heapSortAscending(nums, n);
+    printNums(nums);
+    heapSortDescending(nums, n);
+    printNums(nums);
 }
