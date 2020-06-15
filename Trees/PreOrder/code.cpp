b57ed80527800fbd62cfa55fafb0c2fa1326fc68
palash04 <palashkamble22@gmail.com>
2020-06-15 19:26:14 +0530

    Create code.cpp

---
+++ Trees/PreOrder/code.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left,*right;
+    TreeNode(int x): val(x),left(NULL),right(NULL) {}
+};
+
+void preorder(TreeNode *root){
+    if (root){
+        cout << root->val << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void preorderIterative(TreeNode *root){
+    stack<TreeNode*> st;
+    while (true){
+        while (root) {
+            cout << root->val << " ";
+            st.push(root);
+            root = root->left;
+        }
+        if (st.empty()){
+            break;
+        }
+        root = st.top();
+        st.pop();
+        root = root->right;
+    }
+}
+
+int main(){
+    
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(12);
+    root->left->right = new TreeNode(8);
+    root->right->right = new TreeNode(11);
+
+    preorder(root);
+    cout << "\n";
+    preorderIterative(root);
+    cout << "\n";
+}
