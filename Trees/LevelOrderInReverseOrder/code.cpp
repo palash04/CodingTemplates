1d75e22327ce1a2e4941739bd55ceccae476a3a7
palash04 <palashkamble22@gmail.com>
2020-06-26 06:00:49 +0530

    Create code.cpp

---
+++ Trees/LevelOrderInReverseOrder/code.cpp
@@ -0,0 +1,59 @@
+/*
+
+      1
+     / \
+    2   3
+   / \ / \
+  4  5 6  7
+  
+  output : 4 5 6 7 3 2 1
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left,*right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    //O(n),O(n)
+    void levelOrderInReverseOrder(TreeNode *root){
+        stack<TreeNode*> st;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()){
+            TreeNode *curr = q.front();
+            q.pop();
+            if (curr->right){
+                q.push(curr->right);
+            }
+            if (curr->left){
+                q.push(curr->left);
+            }
+            st.push(curr);
+        }
+        while(!st.empty()){
+            cout << st.top()->val << " ";
+            st.pop();
+        }
+    }
+};
+
+int main(){
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    
+    Solution s;
+    s.levelOrderInReverseOrder(root);
+    cout << "\n";
+}
