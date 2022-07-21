5f9df33a4687551252f716a0e6906a587857520f
palash04 <palashkamble22@gmail.com>
2022-07-21 16:57:54 +0530

    Update code.cpp

--- KMP/Leetcode 28. Implement strStr()/code.cpp
+++ KMP/Leetcode 28. Implement strStr()/code.cpp
@@ -1,53 +1,70 @@
 class Solution {
 public:
     
-    void createLPS(vector<int> &lps,string pat,int n){
-        lps[0] = -1;
-        for (int i=1;i<n;i++){
-            if (pat[lps[i-1]+1] == pat[i]){
-                lps[i] = lps[i-1]+1;
-            }else{
-                if (pat[i-1] == pat[i]){
-                    lps[i] = lps[i-1];
-                }else{
-                    lps[i] = -1;
+    void computeLPS(string p, int n, vector<int> &lps) {
+        
+        lps[0] = 0;
+        
+        for (int i=1;i<n;i++) {
+            if (lps[i-1] == 0) {
+                if (p[i] == p[0]) {
+                    lps[i] = 1;
+                } else {
+                    lps[i] = 0;
+                }
+            } else {
+                if (p[lps[i-1]] == p[i]) {
+                    lps[i] = lps[i-1] + 1;
+                } else {
+                    
+                    if (p[i] == p[i-1] and p[i] == p[0]) lps[i] = lps[i-1];
+                    else lps[i] = 0;
                 }
             }
         }
     }
     
-    int kmp(const vector<int> &lps,string txt,string pat,int m,int n){
-        int i=0;
-        int j = -1;
-        while (i < m){
-            if (txt[i] == pat[j+1]){
-                i++;
-                j++;
-            }else{
-                if (j != -1){
-                    j = lps[j];
-                }else{
-                    i++;
+    int search(string txt, string p, int m, int n, vector<int> &lps) {
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < m) {
+            if (txt[i] == p[j]) {
+                i += 1;
+                j += 1;
+                
+                if (j == n) {
+                    return i - j;
+                    // count all occurrences
+                    j = lps[j-1];
                 }
-            }
-            if (j == n-1){
-                return i-n;
+            } else if (i < m and txt[i] != p[j]) {
+                if (j > 0) {
+                    j = lps[j-1];
+                } else {
+                    i = i + 1;
+                }
             }
         }
         return -1;
     }
     
-    int strStr(string haystack, string needle) {
-        string txt = haystack;
-        string pat = needle;
-        
+    int strStr(string txt, string p) {
         int m = (int)txt.size();
-        int n = (int)pat.size();
+        int n = (int)p.size();
         
         if (n == 0) return 0;
+        if (n > m) return -1;
         
         vector<int> lps(n);
-        createLPS(lps,pat,n);
-        return kmp(lps,txt,pat,m,n);
+        
+        computeLPS(p, n, lps);
+        
+        for (auto v : lps) {
+            cout << v << " ";
+        }
+        
+        return search(txt, p, m, n, lps);
     }
 };
